14dee38993f3b2f965ac7408a1f12d3ffc265e70
kimmincheols <look753@naver.com>
2022-08-05 16:03:23 +0900

    Calc.Java

---
+++ src/Calc.java
@@ -0,0 +1,36 @@
+
+public class Calc {
+	private Student [] array;
+
+	public Calc(Student[] array) {
+		this.array = array;
+	}
+	
+	public void calc( int count ) {
+		for(int i = 0; i < count; i++) {
+			Student student = this.array[i];
+			int total = this.calcTotal(student.getKor(), student.getEng(), student.getMat(),student.getEdp());
+			double avg = this.calcAvg(total, 4);
+			char grade = this.calcGrade(avg);
+			student.setTot(total);
+			student.setAvg(avg);
+			student.setGrad(grade);
+			
+		}
+	}
+	
+	private int calcTotal(int kor, int eng, int mat, int dep) {
+		return kor+eng+mat+dep;
+	}
+	
+	private double calcAvg(int total, int su) {
+		return total / (double)su;
+	}
+	
+	private char calcGrade(double avg) {
+		return (avg >= 90 && avg <= 100) ? 'A' :
+						(avg >= 80 ) ? 'B' :
+							(avg >= 70) ? 'C' :
+									(avg >= 60) ? 'D' : 'F';
+	}
+}

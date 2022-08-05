205cc249e9d4bcb51e82931c3b3330dc3745af68
kimmincheols <look753@naver.com>
2022-08-05 16:48:26 +0900

    Main.Java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("성적관리 프로그램을 시작함둥");
+		Student [] array = new Student[100];
+		Input input = new Input(array);
+		int count = input.input();
+		
+		Calc calc = new Calc(array);
+		calc.calc(count);
+		Sort sort = new Sort(array, count);
+		sort.bubbleSort();
+		
+		Output output = new Output();
+		output.print(array, count);
+		System.out.println("성적관리 프로그램이 끝임둥");
+	}
+}
